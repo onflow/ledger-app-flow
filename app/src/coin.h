72c05c0c45ee30a8a7bd103d6667b90f46e9c413
Juan Leni <juan.leni@zondax.ch>
2020-07-27 17:21:45 +0200

    update bip44 value

--- app/src/coin.h
+++ app/src/coin.h
@@ -27,7 +27,7 @@ extern "C" {
 #define HDPATH_LEN_DEFAULT   5
 
 #define HDPATH_0_DEFAULT     (0x80000000u | 0x2cu)
-#define HDPATH_1_DEFAULT     (0x80000000u | 0x1u)       // FIXME: Adjust after registration
+#define HDPATH_1_DEFAULT     (0x80000000u | 0x21bu)
 #define HDPATH_2_DEFAULT     (0x80000000u | 0u)
 #define HDPATH_3_DEFAULT     (0u)
 #define HDPATH_4_DEFAULT     (0u)
