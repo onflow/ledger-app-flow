1f150723bab67024e3544caf056cbb69243d1d69
Peter Siemens <peterjsiemens@gmail.com>
2020-09-24 00:39:38 -0700

    Compare all token transfer hashes

--- app/src/parser_impl.c
+++ app/src/parser_impl.c
@@ -251,7 +251,10 @@ parser_error_t _matchScriptType(uint8_t scriptHash[32], script_type_e *scriptTyp
         return parser_unexpected_error;
     }
 
-    if (MEMCMP(CONTRACT_HASH_TOKEN_TRANSFER_EMULATOR, buffer, 64) == 0) {
+    if (
+        (MEMCMP(CONTRACT_HASH_TOKEN_TRANSFER_EMULATOR, buffer, 64) == 0) ||
+        (MEMCMP(CONTRACT_HASH_TOKEN_TRANSFER_TESTNET, buffer, 64) == 0) ||
+        (MEMCMP(CONTRACT_HASH_TOKEN_TRANSFER_MAINNET, buffer, 64) == 0)) {
         *scriptType = script_token_transfer;
         return parser_ok;
     }
