96662fa78dedea5d9e95c813bf868a89355da23e
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2021-11-21 22:02:46 +0100

    Updating speculos tests to getpubkey revert

--- tests_speculos/test-basic-get-address-secp256r1.js
+++ tests_speculos/test-basic-get-address-secp256r1.js
@@ -13,55 +13,29 @@ const FlowApp = OnflowLedgerMod.default;
 const app = new FlowApp(common.mockTransport);
 
 console.log(common.humanTime() + " // using FlowApp below with common.mockTransport() to grab apdu command without sending it");
-const slot = 2;
-const address = "e467b9dd11fa00df"
 const scheme = FlowApp.Signature.P256 | FlowApp.Hash.SHA2_256;
 const path = `m/44'/539'/${scheme}'/0/0`;
 
 console.log(common.humanTime() + " // screen shot before sending first apdu command");
 common.curlScreenShot(scriptName);
 
-//set slot
-common.testStep(" - - -", "app.setSlot() // slot=" + slot  + " address=" + address + " path=" + path);
-const setSlotPromise =  app.setSlot(slot , address, path);
-common.testStep("   +  ", "buttons");
-common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Set Account 2");
-common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Account e467..");
-common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Path 44'/..");
-common.curlScreenShot(scriptName); common.curlButton('both', "; confirm; Approve");
-common.curlScreenShot(scriptName); console.log(common.humanTime() + " // back to main screen");
-const setSlotResponse = await setSlotPromise
-assert.equal(setSlotResponse.returnCode, 0x9000);
-
-assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
-assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
-var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
-var hexExpected = "331200001d02e467b9dd11fa00df2c0000801b020080010300800000000000000000";
-common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, slot:1, slotBytes:28, unexpected:9999});
-var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
-var hexExpected = "9000";
-common.compare(hexIncomming, hexExpected, "apdu response", {returnCode:2, unexpected:9999});
-
 //get pubkey
-common.testStep(" - - -", "await app.getAddressAndPubKey() // slot=" + slot);
-const getPubkeyResponse = await app.getAddressAndPubKey(slot);
+common.testStep(" - - -", "await app.getAddressAndPubKey() // slot=" + path);
+const getPubkeyResponse = await app.getAddressAndPubKey(path);
 assert.equal(getPubkeyResponse.returnCode, 0x9000);
 assert.equal(getPubkeyResponse.errorMessage, "No errors");
-const expected_address_string = "e467b9dd11fa00df";
 const expected_pk = "04db0a14364e5bf43a7ddda603522ddfee95c5ff12b48c49480f062e7aa9d20e84215eef9b8b76175f32802f75ed54110e29c7dc76054f24c028c312098e7177a3";
-assert.equal(getPubkeyResponse.address, expected_address_string);
+assert.equal(getPubkeyResponse.address, expected_pk);
 assert.equal(getPubkeyResponse.publicKey.toString('hex'), expected_pk);
 
 assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
 assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
 var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
-var hexExpected = "330100000102";
-common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, slot:1, unexpected:9999});
+var hexExpected = "3301000014xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, do_not_compare_path:20, unexpected:9999});
 var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
-var hexExpected = "e467b9dd11fa00df04db0a14364e5bf43a7ddda603522ddfee95c5ff12b48c49480f062e7aa9d20e84215eef9b8b76175f32802f75ed54110e29c7dc76054f24c028c312098e7177a39000";
-common.compare(hexIncomming, hexExpected, "apdu response", {address:8, publicKey:65, returnCode:2, unexpected:9999});
-
-//TODO We should undo set slot here
+var hexExpected = "04db0a14364e5bf43a7ddda603522ddfee95c5ff12b48c49480f062e7aa9d20e84215eef9b8b76175f32802f75ed54110e29c7dc76054f24c028c312098e7177a3303464623061313433363465356266343361376464646136303335323264646665653935633566663132623438633439343830663036326537616139643230653834323135656566396238623736313735663332383032663735656435343131306532396337646337363035346632346330323863333132303938653731373761339000";
+common.compare(hexIncomming, hexExpected, "apdu response", {publicKey:65, publicKey_hex:130, returnCode:2, unexpected:9999});
 
 common.testEnd(scriptName);
 
--- tests_speculos/test-basic-slot-status-full.js
+++ tests_speculos/test-basic-slot-status-full.js
@@ -23,7 +23,7 @@ assert.equal(slotStatusResponse.returnCode, 0x9000);
 assert.equal(slotStatusResponse.errorMessage, "No errors");
 let expectedBuffer = Buffer.alloc(64);
 expectedBuffer.fill(0);
-//assert.equal(slotStatusResponse.status, expectedBuffer);
+assert.equal(slotStatusResponse.status.toString("hex"), expectedBuffer.toString("hex"));
 
 assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
 assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
@@ -33,23 +33,6 @@ common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2
 var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
 var hexExpected = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009000";
 common.compare(hexIncomming, hexExpected, "apdu response", {slotStatus:64, returnCode:2, unexpected:9999});
-
-
-//getSlot for an empty slot
-const emptySlot = 3;
-common.testStep(" - - -", "await app.getSlot() // emptySlot=" + emptySlot + "; Get empty slot should error");
-const getSlotResponse = await app.getSlot(emptySlot);
-assert.equal(getSlotResponse.returnCode, 0x6982);
-assert.equal(getSlotResponse.errorMessage, "Empty Buffer");
-
-assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
-assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
-var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
-var hexExpected = "331100000103";
-common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, slot:1, unexpected:9999});
-var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
-var hexExpected = "6982";
-common.compare(hexIncomming, hexExpected, "apdu response", {returnCode:2, unexpected:9999});
 
 //setSlot 10
 const expectedSlot = 10;
@@ -98,10 +81,10 @@ common.compare(hexIncomming, hexExpected, "apdu response", {slotStatus:64, retur
 
 //test get slot on used slot 10
 common.testStep(" - - -", "await app.getSlot() // expectedSlot=" + expectedSlot + "; Get slot 10 back");
-const getSlotResponse2 = await app.getSlot(expectedSlot);
-assert.equal(getSlotResponse2.returnCode, 0x9000);
-assert.equal(getSlotResponse2.account, expectedAccount);
-assert.equal(getSlotResponse2.path, expectedPath);
+const getSlotResponse = await app.getSlot(expectedSlot);
+assert.equal(getSlotResponse.returnCode, 0x9000);
+assert.equal(getSlotResponse.account, expectedAccount);
+assert.equal(getSlotResponse.path, expectedPath);
 
 assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
 assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
@@ -112,11 +95,88 @@ var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
 var hexExpected = "e467b9dd11fa00df2c0000801b0200800102008000000000000000009000";
 common.compare(hexIncomming, hexExpected, "apdu response", {account:8, path:20, returnCode:2, unexpected:9999});
 
+//getSlot for an empty slot
+const emptySlot = 3;
+common.testStep(" - - -", "await app.getSlot() // emptySlot=" + emptySlot + "; Get empty slot should error");
+const getSlotResponse2 = await app.getSlot(emptySlot);
+assert.equal(getSlotResponse2.returnCode, 0x6982);
+assert.equal(getSlotResponse2.errorMessage, "Empty Buffer");
+
+assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
+assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
+var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
+var hexExpected = "331100000103";
+common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, slot:1, unexpected:9999});
+var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
+var hexExpected = "6982";
+common.compare(hexIncomming, hexExpected, "apdu response", {returnCode:2, unexpected:9999});
+
+//setSlot 10 - update slot
+const expectedAccount2 = "e467b9dd11fa00de"; //this is not a proper account but the app does not test it
+const expectedPath2 = `m/44'/539'/${scheme}'/0/1`;
+common.testStep(" - - -", "app.setSlot() // expectedSlot=" + expectedSlot + " expectedAccount2=" + expectedAccount2 + " expectedPath2=" + expectedPath2 + "; Set slot 10");
+const setSlotPromise2 = app.setSlot(expectedSlot, expectedAccount2, expectedPath2);
+common.testStep("   +  ", "buttons");
+common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Update Account 10");
+common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Old Account e467..");
+common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Old Path 44'/..");
+common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; New Account e467..");
+common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; New Path 44'/..");
+common.curlScreenShot(scriptName); common.curlButton('both', "; confirm; Approve");
+common.curlScreenShot(scriptName); console.log(common.humanTime() + " // back to main screen");
+common.testStep(" - - -", "await setSlotPromise // expectedSlot=" + expectedSlot + " expectedAccount2=" + expectedAccount2 + " expectedPath=2" + expectedPath2 + "; Set slot 10");
+const setSlotResponse2 = await setSlotPromise2
+assert.equal(setSlotResponse2.returnCode, 0x9000);
+
+assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
+assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
+var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
+var hexExpected = "331200001d0ae467b9dd11fa00de2c0000801b020080010200800000000001000000";
+common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, slot:1, slotBytes:28, unexpected:9999});
+var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
+var hexExpected = "9000";
+common.compare(hexIncomming, hexExpected, "apdu response", {returnCode:2, unexpected:9999});
+
+//test slotstatus when slot 10 is used
+common.testStep(" - - -", "await app.slotStatus() // Get slot status");
+const slotStatusResponse3 = await app.slotStatus();
+assert.equal(slotStatusResponse3.returnCode, 0x9000);
+assert.equal(slotStatusResponse3.errorMessage, "No errors");
+expectedBuffer = Buffer.alloc(64);
+expectedBuffer.fill(0);
+expectedBuffer[10] = 1;
+assert.equal(slotStatusResponse3.status.toString("hex"), expectedBuffer.toString("hex"));
+
+assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
+assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
+var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
+var hexExpected = "3310000000";
+common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, unexpected:9999});
+var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
+var hexExpected = "000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009000";
+common.compare(hexIncomming, hexExpected, "apdu response", {slotStatus:64, returnCode:2, unexpected:9999});
+
+//test get slot on used slot 10
+common.testStep(" - - -", "await app.getSlot() // expectedSlot=" + expectedSlot + "; Get slot 10 back");
+const getSlotResponse3 = await app.getSlot(expectedSlot);
+assert.equal(getSlotResponse3.returnCode, 0x9000);
+assert.equal(getSlotResponse3.account, expectedAccount2);
+assert.equal(getSlotResponse3.path, expectedPath2);
+
+assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
+assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
+var hexOutgoing = common.mockTransport.hexApduCommandOut.shift();
+var hexExpected = "33110000010a";
+common.compare(hexOutgoing, hexExpected, "apdu command", {cla:1, ins:1, p1:1, p2:1, len:1, slot:1, unexpected:9999});
+var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
+var hexExpected = "e467b9dd11fa00de2c0000801b0200800102008000000000010000009000";
+common.compare(hexIncomming, hexExpected, "apdu response", {account:8, path:20, returnCode:2, unexpected:9999});
+
 //Now delete the slot so that the next test starts in a clean state
 const expectedAccountDelete = "0000000000000000";
 const expectedPathDelete = `m/0/0/0/0/0`;
 common.testStep(" - - -", "app.setSlot() // expectedSlot=" + expectedSlot + " expectedAccountDelete=" + expectedAccountDelete + " expectedPathDelete=" + expectedPathDelete + "; Delete slot 10");
-const setSlotPromise2 = app.setSlot(10, expectedAccountDelete, expectedPathDelete);
+const setSlotPromise3 = app.setSlot(10, expectedAccountDelete, expectedPathDelete);
 common.testStep("   +  ", "buttons");
 common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Delete Account 10");
 common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the address / path; Old Account e467..");
@@ -124,8 +184,8 @@ common.curlScreenShot(scriptName); common.curlButton('right', "; navigate the ad
 common.curlScreenShot(scriptName); common.curlButton('both', "; confirm; Approve");
 common.curlScreenShot(scriptName); console.log(common.humanTime() + " // back to main screen");
 common.testStep(" - - -", "await setSlotPromise2 // expectedSlot=" + expectedSlot + " expectedAccountDelete=" + expectedAccountDelete + " expectedPathDelete=" + expectedPathDelete + "; Delete slot 10");
-const setSlotResponse2 = await setSlotPromise2;
-assert.equal(setSlotResponse2.returnCode, 0x9000);
+const setSlotResponse3 = await setSlotPromise3;
+assert.equal(setSlotResponse3.returnCode, 0x9000);
 
 assert.equal(common.mockTransport.hexApduCommandOut.length, 1)
 assert.equal(common.mockTransport.hexApduCommandIn.length, 1)
