3f656571bdb64d656c4c3165dd3afdd3c940b136
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2021-12-05 23:09:31 +0100

    Remove sleep 3 in the speculos tests

--- tests_speculos/test-basic-show-address-expert.js
+++ tests_speculos/test-basic-show-address-expert.js
@@ -52,4 +52,9 @@ var hexIncomming = common.mockTransport.hexApduCommandIn.shift();
 var hexExpected = "04d7482bbaff7827035d5b238df318b10604673dc613808723efbd23fbc4b9fad34a415828d924ec7b83ac0eddf22ef115b7c203ee39fb080572d7e51775ee54be303464373438326262616666373832373033356435623233386466333138623130363034363733646336313338303837323365666264323366626334623966616433346134313538323864393234656337623833616330656464663232656631313562376332303365653339666230383035373264376535313737356565353462659000";
 common.compare(hexIncomming, hexExpected, "apdu response", {publicKey:65, publicKey_hex:130, returnCode:2, unexpected:9999});
 
+//undo expert mode
+common.testStep("   +  ", "buttons; enable expert mode");
+common.curlButton('right', "; Expert mode: enabled"); common.curlScreenShot(scriptName);
+common.curlButton('both' , "; Expert mode: disabled" ); common.curlScreenShot(scriptName);
+common.curlButton('left' , "; Flow Ready"           ); common.curlScreenShot(scriptName);
 common.testEnd(scriptName);
